b95ccb1e6b53d4e86b958115cfb4671c0282a027
Michael Bar-Sinai <mich.barsinai@gmail.com>
2019-01-20 00:30:44 +0200

    Updated requirement to look at the logic requests

--- SimSat/src/main/resources/SimSatRequirements.js
+++ SimSat/src/main/resources/SimSatRequirements.js
@@ -1,32 +1,33 @@
-/* global bp */
+/* global bp, EPSTelem, EPSTelemetry */
 
 /**
  * Raise a false assrtion when the satellite is in
  * SetADCSModePayloadPointing and not in SetEPSModeGood
  */
-bp.registerBThread("NeverPointingOnLow", function(){
-  var relevantEvents = [EPSTelem, ADCSTelem];
-  /* Init */
-  var canPoint;
-  var isPointing;
-  var evt = bp.sync({waitFor:relevantEvents});
-  if ( ADCSTelem.contains(evt) ) {
-    isPointing = evt.mode.equals( ADCSMode.PayloadPointing );
-  } else if ( EPSTelem.contains(evt) ) {
-    canPoint = evt.mode.equals( EPSTelemetry.EPSMode.Good );
-  }
 
-  /* ongoing verification */
-  while ( true ) {
-    var evt = bp.sync({waitFor:relevantEvents});
-    if ( ADCSTelem.contains(evt) ) {
-      isPointing = evt.mode.equals( ADCSTelemetry.ADCSMode.PayloadPointing );
-    } else if ( EPSTelem.contains(evt) ) {
-      canPoint = evt.mode.equals( EPSTelemetry.EPSMode.Good );
+var USE_PAYLOAD = bp.Event("SetADCSModePayloadPointing");
+
+bp.registerBThread("NeverPointingOnLow", function () {
+    var relevantEvents = [EPSTelem, USE_PAYLOAD];
+    /* Init */
+    var canPoint;
+    var evt = bp.sync({waitFor: relevantEvents});
+    if (EPSTelem.contains(evt)) {
+        canPoint = evt.mode.equals(EPSTelemetry.EPSMode.Good);
     }
-             
-    bp.ASSERT(!(isPointing && !canPoint), 
-             "Satellite is using the payload while it should conserve energy" );
-  }
+
+    /* ongoing verification */
+    while (true) {
+        var pointingRequested = false;
+        var evt = bp.sync({waitFor: relevantEvents});
+        if (EPSTelem.contains(evt)) {
+            canPoint = evt.mode.equals(EPSTelemetry.EPSMode.Good);
+        } else if (USE_PAYLOAD.equals(evt)) {
+            pointingRequested = true;
+        }
+
+        bp.ASSERT(!(pointingRequested && !canPoint),
+                "Satellite is using the payload while it should conserve energy");
+    }
 });
 
